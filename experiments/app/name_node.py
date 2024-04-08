505ae8e6d58ef5f596b5c60662b8d70f8ada9b47
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2024-04-07 23:43:10 -0500

    Refactor NameNode class in experiments/app/name_node.py

--- experiments/app/name_node.py
+++ experiments/app/name_node.py
@@ -1,10 +1,14 @@
 class NameNode:
     def __init__(self) -> None:
-        self.metadata = {}
-        self.data_nodes = []
+        self.metadata = {}  # maps file names to lists of (block_id, data_node) tuples
+        self.data_nodes = []  # stores DataNode addresses
 
     def register_data_node(self, data_node_address: str) -> None:
-        self.data_nodes.append(data_node_address)
+        if data_node_address not in self.data_nodes:
+            self.data_nodes.append(data_node_address)
 
-    def store_file_metadata(self, file_name: str, data_blocks) -> None:
+    def store_file_metadata(self, file_name: str, data_blocks: list) -> None:
         self.metadata[file_name] = data_blocks
+
+    def get_file_metadata(self, file_name: str) -> list:
+        return self.metadata.get(file_name, [])
